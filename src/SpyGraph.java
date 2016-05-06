f1e28903b6d55a7ca18b1b082a4e19672d6eba70
edegler <evandegler1@gmail.com>
2016-05-05 20:18:09 -0500

    we're very close

--- src/SpyGraph.java
+++ src/SpyGraph.java
@@ -68,34 +68,35 @@ public class SpyGraph implements Iterable<GraphNode> {
 		// Add the start node to the queue
 		queue.add(getNodeFromName(start));
 
+		visited.add(new Neighbor(0, getNodeFromName(start)));
+		
 		// While the queue isn't empty
 		while (!queue.isEmpty()) {
 
 			// Remove the first node from the queue
 			GraphNode curr = queue.remove();
 
+			List<Neighbor> nList = new ArrayList<Neighbor>(curr.getNeighbors());
+
+
+			//sortByCost(nList);
+			//Collections.sort(nList, new CostComparator());
+
 			// For of curr's neighbors
-			for (Neighbor succ : curr.getNeighbors()) {
+			for (Neighbor succ : nList) {
 
-				boolean contains = false;
-
-				// For each neighbor in visited
-				for (Neighbor v : visited) {
-
-					// If any of the nodes in visited equal the curr node,
-					// set contains to true
-					if (v.getNeighborNode().getNodeName().equals(succ.getNeighborNode().getNodeName())) {
-						contains = true;
-					}
-				}
-				// If we didn't find a match in visited, keep going
-				if (contains) continue;
+				
+				// If we didn't find a match in alreadyVisited, keep going
+				if (alreadyVisited(succ, visited)) continue;
 
 				// Add the succ node to the visited list
 				visited.add(succ);
 
 				// If succ equals the end node, return the visited list
-				if (succ.getNeighborNode().getNodeName().equals(end)) return visited;
+				if (succ.getNeighborNode().getNodeName().equals(end)){
+					visited.remove(0);
+					return visited;
+				}
 
 				// Add succ to the queue
 				queue.add(succ.getNeighborNode());
@@ -103,7 +104,42 @@ public class SpyGraph implements Iterable<GraphNode> {
 		}
 
 		// never got to end... no path exists
-		return new ArrayList<Neighbor>();
+		visited.remove(0);
+		return visited;
+		//return new ArrayList<Neighbor>();
+	}
+
+	private boolean alreadyVisited(Neighbor n, List<Neighbor> visited){
+		boolean contains = false;
+
+		// For each neighbor in visited
+		for (Neighbor v : visited) {
+
+			// If any of the nodes in visited equal the curr node,
+			// set contains to true
+			if (v.getNeighborNode().getNodeName().equals(n.getNeighborNode().getNodeName())) {
+				contains = true;
+			}
+		}
+		return contains;
+	}
+	
+	private void sortByCost(List<Neighbor> nList) {
+		for (int i = 1; i < nList.size(); i++){
+			Neighbor temp = nList.get(i);
+
+			int j;
+			for (j = i-1; j >= 0 && nList.get(j).getCost() > temp.getCost(); j--){
+				Neighbor jayPlusOne = nList.get(j+1);
+				Neighbor jay = nList.get(j);
+				nList.remove(j);
+				nList.add(j, jayPlusOne);
+				nList.remove(j+1);
+				nList.add(j+1, jay);
+			}
+			nList.remove(j+1);
+			nList.add(j+1, temp);
+		}
 	}
 
 	/**
@@ -117,99 +153,124 @@ public class SpyGraph implements Iterable<GraphNode> {
 		return null;
 	}
 
-//	/**
-//	 * Return Depth First Search list of nodes on path 
-//	 * from one Node to another.
-//	 * @param start First node in DFS traversal
-//	 * @param end Last node (match node) in DFS traversal
-//	 * @return The DFS traversal from start to end node.
-//	 */
-//	public List<Neighbor> DFS2(String start, String end) {
-//
-//		List<Neighbor> visited = new ArrayList<Neighbor>();
-//
-//		DFS2(end, getNodeFromName(start), getNodeFromName(start).getNeighbors().get(0), visited);
-//
-//		return visited;
-//	}
-//
-//
-//	private void DFS2(String end, GraphNode currNode, Neighbor currNeighbor, List<Neighbor> visited){
-//
-//		visited.add(currNeighbor);
-//		if (currNeighbor.getNeighborNode().getNodeName().equals(end)) return;
-//		//if (currNode.getNodeName().equals(end)) return;
-//
-//		for (Neighbor n : currNeighbor.getNeighborNode().getNeighbors()){
-//			boolean contains = false;
-//			for (Neighbor v : visited){
-//				if (v.getNeighborNode().getNodeName().equals(n.getNeighborNode().getNodeName()))
-//					contains = true;
-//			}
-//
-//			if (!contains && !currNode.getNodeName().equals(end))
-//				DFS2(end, currNeighbor.getNeighborNode(), n, visited);
-//		}
-//	}
-
-
-
-
-
+	/**
+	 * Return Depth First Search list of nodes on path 
+	 * from one Node to another.
+	 * @param start First node in DFS traversal
+	 * @param end Last node (match node) in DFS traversal
+	 * @return The DFS traversal from start to end node.
+	 */
 	public List<Neighbor> DFS(String start, String end) {
-		// TODO - fix this yo
-		System.out.println("Enters DFS");
+
 		List<Neighbor> visited = new ArrayList<Neighbor>();
-		Stack<GraphNode> stack = new Stack<GraphNode>();
-		
-		stack.push(getNodeFromName(start));
 
-		while (!stack.isEmpty()){
-			GraphNode node = stack.peek();
-			System.out.println("Node is " + node.getNodeName());
-			Neighbor nextNeighbor = nextNeighbor(node, visited);
-			
-			// THIS SHOULD PRINT OUT A
-			System.out.println("nextNeighbor is " + nextNeighbor.getNeighborNode().getNodeName());
-			
-			if (nextNeighbor != null) {
-				visited.add(nextNeighbor);
-				
-				// stop if we get to end
-				if (nextNeighbor.getNeighborNode().getNodeName().equals(end)) {
-					return visited;
-				}
-				
-				stack.push(nextNeighbor.getNeighborNode());
-			} else {
-				stack.pop();
+		DFS(end, getNodeFromName(start), getNodeFromName(start).getNeighbors().get(0), visited);
+
+		List<Neighbor> ret = new ArrayList<Neighbor>();
+		int endIndex = 0;
+		for (Neighbor n : visited){
+			if (n.getNeighborNode().getNodeName().equals(end)){
+				break;
 			}
-			
+			endIndex++;
 		}
-
-		return new ArrayList<Neighbor>();
+		while (endIndex >= 0){
+			Neighbor v = visited.get(endIndex--);
+			if (v.getNeighborNode().getNodeName().equals(start)) break;
+			ret.add(0, v);
+		}
+		return ret;
 	}
-	
-	private Neighbor nextNeighbor(GraphNode node, List<Neighbor> visited) {
-//		List<Neighbor> neighborsCopy = new ArrayList<Neighbor>(node.getNeighbors());
-//		Collections.sort(neighborsCopy, c);
-		
-		for (Neighbor adj : node.getNeighbors()) {
+
+
+	private void DFS(String end, GraphNode currNode, Neighbor currNeighbor, List<Neighbor> visited){
+
+		visited.add(currNeighbor);
+		if (currNeighbor.getNeighborNode().getNodeName().equals(end)) return;
+		//if (currNode.getNodeName().equals(end)) return;
+
+		for (Neighbor n : currNeighbor.getNeighborNode().getNeighbors()){
 			boolean contains = false;
-			
+			for (Neighbor v : visited){
+				if (v.getNeighborNode().getNodeName().equals(n.getNeighborNode().getNodeName()))
+					contains = true;
+			}
+
+			if (!contains)// && !currNode.getNodeName().equals(end))
+				DFS(end, currNeighbor.getNeighborNode(), n, visited);
+		}
+	}
+
+	//	public List<Neighbor> DFS(String start, String end) {
+	//		// TODO - fix this
+	//		
+	//		// Creates a list to hold all visited neighbors and
+	//		// a stack to put GraphNodes in to be taken off
+	//		List<Neighbor> visited = new ArrayList<Neighbor>();
+	//		Stack<GraphNode> stack = new Stack<GraphNode>();
+	//		
+	//		//List<Neighbor> ret = new ArrayList<Neighbor>();
+	//		
+	//		// Add the start node to the stack
+	//		stack.push(getNodeFromName(start));
+	//		//visited.add(new Neighbor(0, getNodeFromName(start)));
+	//		
+	//		// While there's still stuff in the stack
+	//		while (!stack.isEmpty()){
+	//
+	//			// node is the GraphNode on the top of the stack
+	//			GraphNode node = stack.peek();
+	//			
+	//			// nextNeighbor is the next unvisited neighbor of node
+	//			Neighbor nextNeighbor = nextNeighbor(node, visited, start);
+	//			
+	//			// If there was a nextNeighbor
+	//			if (nextNeighbor != null) {
+	//				// Add nextNeighbor to the visited list
+	//				visited.add(nextNeighbor);
+	//				
+	//				// Stop if we get to end
+	//				if (nextNeighbor.getNeighborNode().getNodeName().equals(end)) {
+	//					return visited;
+	//				}
+	//				// Push the nextNeighbor onto the stack
+	//				stack.push(nextNeighbor.getNeighborNode());
+	//				
+	//			// If there wasn't a nextNeighbor, pop node off the stack
+	//			} else {
+	//				stack.pop();
+	//			}
+	//			
+	//		}
+	//		
+	//		return new ArrayList<Neighbor>();
+	//	}
+
+
+	private Neighbor nextNeighbor(GraphNode node, List<Neighbor> visited, String start) {
+		//		List<Neighbor> neighborsCopy = new ArrayList<Neighbor>(node.getNeighbors());
+		//		Collections.sort(neighborsCopy, c);
+
+		for (Neighbor adj : node.getNeighbors()) {
+			boolean alreadyVisited = false;
+
 			for (Neighbor v: visited) {
 				if (v.getNeighborNode().getNodeName().equals(adj.getNeighborNode().getNodeName())) {
-					contains = true;
+					alreadyVisited = true;
 					break;
 				}
 			}
-			
-			if (!contains) return adj;
+			// Shouldn't this be:
+			// if (contains) return adj;
+			// because if it DOES contain it, we want to return it
+			if (!alreadyVisited && !adj.getNeighborNode().getNodeName().equals(start)){
+				return adj;
+			}
 		}
-		
+		// If NO NODE was found, don't we then return null -- nothing was found
 		return null;
 	}
-	
+
 
 
 
@@ -243,6 +304,10 @@ public class SpyGraph implements Iterable<GraphNode> {
 		int randomNodeIndex = Game.RNG.nextInt(vlist.size());
 		return vlist.get(randomNodeIndex);
 	}
-
-
+}
+class CostComparator implements Comparator<Neighbor> {
+    @Override
+    public int compare(Neighbor a, Neighbor b) {
+        return a.getCost() - b.getCost();
+    }
 }
